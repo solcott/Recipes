6f0f557b2d55402bd31357cdd9c5155f91c960b2
scottolcott <scottolcott@gmail.com>
2026-05-05 10:26:18 -0600

    Clean up trailing whitespace and standardize file formatting.
    * **Refactoring**: Updated `AndroidAppGraph.kt`, `RecipeApplication.kt`, and `RecipeApp.android.kt` to ensure consistent end-of-file formatting and remove unnecessary whitespace.

--- repository/src/commonMain/kotlin/com/scottolcott/recipe/repository/RepositoryProviders.kt
+++ repository/src/commonMain/kotlin/com/scottolcott/recipe/repository/RepositoryProviders.kt
@@ -3,4 +3,4 @@ package com.scottolcott.recipe.repository
 import dev.zacsweers.metro.AppScope
 import dev.zacsweers.metro.ContributesTo
 
-@ContributesTo(AppScope::class) interface RepositoryProviders {}
+@ContributesTo(AppScope::class) interface RepositoryProviders
