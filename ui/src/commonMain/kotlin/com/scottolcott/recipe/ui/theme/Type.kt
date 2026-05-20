33d22e3f159e126311863574b53d035fa9983967
scottolcott <scottolcott@gmail.com>
2026-05-20 09:56:23 -0600

    Set explicit default font family for AppTypography.
    * **UI**: Updated the `AppTypography` definition in the theme to explicitly reference `FontFamily.Default`.

--- ui/src/commonMain/kotlin/com/scottolcott/recipe/ui/theme/Type.kt
+++ ui/src/commonMain/kotlin/com/scottolcott/recipe/ui/theme/Type.kt
@@ -1,5 +1,6 @@
 package com.scottolcott.recipe.ui.theme
 
 import androidx.compose.material3.Typography
+import androidx.compose.ui.text.font.FontFamily
 
-val AppTypography = Typography()
+val AppTypography = Typography(fontFamily = FontFamily.Default)
