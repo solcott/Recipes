4eb12e704685f7b429da6fbbeb0be3c022f7d84d
Scott Olcott <scottolcott@gmail.com>
2026-06-15 16:03:18 -0600

    Update dependencies and configure repository for Detekt 2.0.0-alpha.4.
    * **Build System**:
        * Updated `detekt` to `2.0.0-alpha.4` and `detekt-compose-rules` to `0.6.1`.
        * Added the Gradle `libs-releases` Maven repository to `build-logic/settings.gradle.kts` as a workaround for a dependency resolution issue in the current Detekt alpha.
        * Updated several library versions in `libs.versions.toml`:
            * `dependency-analysis` to `3.15.0`
            * `metro` to `1.2.1`
            * `store` to `5.1.0-alpha09`
    
    Signed-off-by: Scott Olcott <scottolcott@gmail.com>

--- build-logic/settings.gradle.kts
+++ build-logic/settings.gradle.kts
@@ -12,6 +12,9 @@ dependencyResolutionManagement {
     google()
     mavenCentral()
     gradlePluginPortal()
+    // Needed for 2.0.0-alpha.4.  Can remove once 2.0.0-alpha.5 is released
+    // https://github.com/detekt/detekt/issues/9396
+    maven { url = uri("https://repo.gradle.org/gradle/libs-releases") }
     mavenLocal()
   }
   versionCatalogs { create("libs") { from(files("../gradle/libs.versions.toml")) } }
