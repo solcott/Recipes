e4f1c03f179aff91accd6745efb09b1ba7127793
Scott Olcott <scottolcott@gmail.com>
2026-08-27 16:07:35 -0600

    Refactor ingredients API resource endpoint.
    * **Network**:
        * Created a standalone `IngredientsResource` mapped to the `list.php` endpoint.
        * Deleted the unused `ListResource` and its nested `IngredientsResource` configuration.
        * Updated `IngredientsApiImpl` to use the new top-level `IngredientsResource` instead of the nested one.
    
    Signed-off-by: Scott Olcott <scottolcott@gmail.com>

--- network/src/commonMain/kotlin/com/scottolcott/recipe/network/resource/ListResource.kt
+++
@@ -1,9 +0,0 @@
-package com.scottolcott.recipe.network.resource
-
-import io.ktor.resources.Resource
-
-@Resource("filter.php")
-internal class ListResource {
-
-  data class IngredientsResource(val i: String = "list", val parent: ListResource = ListResource())
-}
