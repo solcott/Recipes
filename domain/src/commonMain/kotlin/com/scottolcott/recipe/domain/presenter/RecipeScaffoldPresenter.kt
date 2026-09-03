13c1fbac510942b6e5e50d6f9b1aac893c5fbe36
Scott Olcott <scottolcott@gmail.com>
2026-09-03 17:51:18 -0600

    Require medium window height to show navigation rail.
    Update `RecipeScaffoldPresenter`'s `showNavRail` condition to check that the window size class height is at least `HEIGHT_DP_MEDIUM_LOWER_BOUND`, in addition to the existing expanded width check. This prevents the navigation rail from appearing on wide but short screens.
    
    Signed-off-by: Scott Olcott <scottolcott@gmail.com>

--- domain/src/commonMain/kotlin/com/scottolcott/recipe/domain/presenter/RecipeScaffoldPresenter.kt
+++ domain/src/commonMain/kotlin/com/scottolcott/recipe/domain/presenter/RecipeScaffoldPresenter.kt
@@ -49,6 +49,7 @@ class RecipeScaffoldPresenter internal constructor(private val navigator: Naviga
     val windowSizeClass = LocalWindowSizeClass.current
     val showNavRail =
       windowSizeClass.isWidthAtLeastBreakpoint(WindowSizeClass.WIDTH_DP_EXPANDED_LOWER_BOUND) &&
+        windowSizeClass.isHeightAtLeastBreakpoint(WindowSizeClass.HEIGHT_DP_MEDIUM_LOWER_BOUND) &&
         !isIos()
 
     var searchBarValue by retain { mutableStateOf(SearchBarValue.Collapsed) }
